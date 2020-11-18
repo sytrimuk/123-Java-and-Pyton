1076e470d105ee4ac23ad72041e30f9ea17bcb93
sytrimuk <2005levon@gmail.com>
2020-11-18 20:24:26 +0300

    Der

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Goodbye America!!");
+    }
+}
+
+/* class as{
+    public static void main(String[] args) {
+        System.out.println("Ilya LOX");
+    }
+ */
